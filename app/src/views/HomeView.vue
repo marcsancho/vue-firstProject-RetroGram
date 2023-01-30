6eefd0c4b7cf458faa7cec3cf1e01c8f0d1a4512
marcsancho <msanchosantandreu@cifpfbmoll.eu>
2023-01-30 23:14:09 +0100

    primer checkpoint

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <Carousel />  
+    <MainContent />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Carousel from '@/components/Carousel.vue'
+import MainContent from '@/components/MainContent.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    Carousel,
+    MainContent
+  }
+}
+</script>
+
+
