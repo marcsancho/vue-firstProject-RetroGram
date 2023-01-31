5a80dedede9341821570095fe3390de487b09110
marcsancho <msanchosantandreu@cifpfbmoll.eu>
2023-01-31 18:01:06 +0100

    proyecto

---
+++ retrogram/src/components/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Carousel from "@/components/HomeComponents/CarouselMain.vue";
+</script>
+
+<template>
+  <Carousel />
+</template>
+
+<script>
+export default {
+  name: "Home",
+  computed: {
+    username() {
+      return this.$route.params.username
+    },
+  },
+  methods: {
+    goToDashboard() {
+      if (isAuthenticated) {
+        this.$router.push('/dashboard')
+      } else {
+        this.$router.push('/login')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
